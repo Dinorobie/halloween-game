89aceee06d9360268c9040c7e88ab97f1ea2ef5a
Roberto Sepulveda <67658859+Robie94@users.noreply.github.com>
2022-10-18 22:10:55 -0500

    Add files via upload

--- proyect/sketch.js
+++ proyect/sketch.js
@@ -1,9 +1,10 @@
-var character, characterimg;
+
+var character;
 var bg,backgroundimg,invisibleGround;
 var evil, evilimg
 
 function preload(){
-    characterimg= loadImage("./assets/char.gif"); 
+    characterimg= loadAnimation("./assets/char.gif");
     backgroundimg=loadImage("./assets/forest.gif");
     evilimg=loadImage("./assets/zombie.gif");
 }
@@ -15,33 +16,57 @@ function setup(){
     bg = createSprite (400,180,600,600);
     bg.addImage("bg",backgroundimg);
     //bg.velocityX=1;
-    bg.velocityX = -(2 + 1)
+    // bg.velocityX = -(2 + 1)
+    bg.velocityX = -1.5;
+
+// Add zombie
+    evil = createSprite(40,400,30,50);
+    evil.addImage("evil",evilimg)
+    evil.scale=0.58;
+   
 
 // Add character
-    character = createSprite(50,400,20,50);
-    character.addImage("character",characterimg);
+    character = createSprite(170,400,30,50);
+    character.addAnimation("character",characterimg);
     character.scale=0.5;
 
+ 
+
 //Add invisible ground
-    invisibleGround = createSprite(600,460,600,590);
+//invisibleGround = createSprite(600,460,600,590);
+    invisibleGround = createSprite(50,450,600,10);
     invisibleGround.visible = false;
 
     
 }
 
 function draw(){
+
+    drawSprites(); 
+    print(character.x)
+    print(character.y)
+
 //moving background
-    if (bg.x > 400){
-        bg.x = 300;
+//if (bg.x > 400){
+//    bg.x = 300;
+//}
+    if (bg.x <= 100){
+        bg.x = 400;
       }
+
+
 //move character
     if(keyDown("space") && character.y>=100){
-        character.velocityY=-12;
+        character.velocityY=-7;
     }
 
+
+
 // add gravity to character+ground
-     character.velocityY = character.velocityY + 0.8
-     character.collide(invisibleGround);
+    character.velocityY = character.velocityY + 0.8
+    character.collide(invisibleGround); 
+    evil.collide(invisibleGround); 
+     // esta linea de codigo se sustituye con los IFs de arriba debido a el bug que tiene la engine p5 con la funcion collide. 
     
-      drawSprites();
+      
  }
